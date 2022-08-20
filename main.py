3eb4b60bab061cb180d0e6cba61aa1ef3534219c
George <mazzeogeorge@gmail.com>
2022-08-20 11:31:12 -0500

    flake8 happy now pre commit hooks

---
+++ main.py
@@ -0,0 +1,10 @@
+def test_function() -> None:
+    """
+    Test function for flake8 the fact that it wants me to write this means its working
+
+    :return:
+    """
+    print("bonjour le monde")
+
+
+test_function()
