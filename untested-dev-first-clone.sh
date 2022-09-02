88e05a6fbb553d8ce7927f2e36fb0ffae9678937
George <mazzeogeorge@gmail.com>
2022-09-02 11:29:04 -0500

    got a nice new shiny terminal let see what it fucks up

--- untested-dev-first-clone.sh
+++ untested-dev-first-clone.sh
@@ -1,6 +1,15 @@
+# pimp your terminal https://www.engineeringwithutsav.com/blog/spice-up-your-macos-terminal
 python -m venv .venv
 source .venv/bin/activate
 pip install -r requirements.txt
+
+# docker
+brew cask install docker
+# gcloud
+brew install --cask google-cloud-sdk
+# terraform
+brew install terraform
+
 
 # test github actions locally
 brew install act
