463f6c5fc03d2acf3950a3b05e6f355e70598580
George Mazzeo <mazzeogeorge@gmail.com>
2023-10-22 14:25:49 -0500

    fixed unit tests

---
+++ fastapi-crud/src/conftest.py
@@ -0,0 +1,29 @@
+from typing import AsyncGenerator
+
+import pytest
+from fastapi import FastAPI
+from httpx import AsyncClient
+from src.api.application import get_app
+
+
+@pytest.fixture
+def fastapi_app() -> FastAPI:
+    """
+    Fixture for creating FastAPI app.
+
+    :return: fastapi app with mocked dependencies.
+    """
+    application = get_app()
+    return application  # noqa: WPS331
+
+
+@pytest.fixture
+async def client(fastapi_app: FastAPI) -> AsyncGenerator[AsyncClient, None]:
+    """
+    Fixture that creates client for requesting server.
+
+    :param fastapi_app: the application.
+    :yield: client for the app.
+    """
+    async with AsyncClient(app=fastapi_app, base_url="http://test") as ac:
+        yield ac
